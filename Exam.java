dce758cd0c2a4a6329fdd970896d5aeed508a36f
narkhederohitk <rohitnarkhede97@gmail.com>
2021-01-15 15:44:47 +0530

    second

---
+++ Exam.java
@@ -0,0 +1,10 @@
+class Exam{
+	public static void main(String args[]){
+	System.out.println("Online Examination");
+	int i;
+		do{
+			System.out.println("1.English /n 2.Math /n 3.GK");
+			i++;
+		}while(i < 10)
+	}
+}
